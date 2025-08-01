ec196285f1ebe5ae72dd4d5891794801a7179c69
Mohammad <bakhshianmo@gmail.com>
2025-08-01 10:57:07 +0330

    add code changes relative Introduction To Form Validation(27)

--- controller/note_create.php
+++ controller/note_create.php
@@ -3,8 +3,17 @@
     require_once "Database.php";
     require_once "Response.php";
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $db = new Database($config['database']);
-        $db->query("INSERT INTO notes (body,user_id) VALUES (:body ,:user_id)" , ['body' => $_POST['body'], 'user_id' => 3 ]);
+        $error =[];
+        if(strlen($_POST["body"]) == 0){
+            $error["body"] = "Body is required";
+        }
+        if(strlen($_POST["body"]) > 1000){
+            $error["body"] = "body is too long ";
+        }
+        if(empty($error)){
+            $db = new Database($config['database']);
+            $db->query("INSERT INTO notes (body,user_id) VALUES (:body ,:user_id)" , ['body' => $_POST['body'], 'user_id' => 3 ]);
+        }
     }
 
     require_once "views/notes_create.view.php";
--- controller/views/notes_create.view.php
+++ controller/views/notes_create.view.php
@@ -9,7 +9,15 @@
                 <div class="col-span-full">
                     <label for="body" class="block text-sm/6 font-medium text-gray-900">Body</label>
                     <div class="mt-2">
-                        <textarea id="body" name="body" placeholder="note text" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                        <textarea id="body"
+                                  name="body"
+                                  placeholder="note text"
+                                  rows="3"
+                                  class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                  ><?= isset($_POST['body']) ? $_POST['body'] : '' ; ?></textarea>
+                        <?php if(isset($error['body'])): ?>
+                            <p class="text-red-500"><?=$error['body'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences  for notes.</p>
                 </div>
