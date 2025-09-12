bebe4ee8c7b1770a0223cc1de6414bab8a5506f3
Mohammad <bakhshianmo@gmail.com>
2025-09-12 17:23:29 +0330

    add code changes Composer and Free Autoloading(47)

--- public/index.php
+++ public/index.php
@@ -2,15 +2,16 @@
     session_start();
     const BASE_PATH = __DIR__ . '/../';
     require_once BASE_PATH . "core/function.php";
-    spl_autoload_register(function ($class) {
-        $class = str_replace("\\", "/", $class);
-        // مسیر فایل کلاس را بر اساس نام کلاس بساز
-        $path = base_path("{$class}.php");
-
-        if (file_exists($path)) {
-            require_once $path;
-        }
-    });
+    require_once BASE_PATH . "vendor/autoload.php";
+//    spl_autoload_register(function ($class) {
+//        $class = str_replace("\\", "/", $class);
+//        // مسیر فایل کلاس را بر اساس نام کلاس بساز
+//        $path = base_path("{$class}.php");
+//
+//        if (file_exists($path)) {
+//            require_once $path;
+//        }
+//    });
     require_once base_path("Bootstrap.php");
 
     $router = new \core\Router();
